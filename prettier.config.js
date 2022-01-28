317dc3f026d6afbf6d979498ad4f51d4ba6de86b
Junan <andyson1024@gmail.com>
2022-01-28 09:50:24 +0800

    feat: update prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  semi: false,
-  singleQuote: true,
-  printWidth: 100,
-  tabWidth: 2,
-  useTabs: false,
-  trailingComma: 'es5',
-  bracketSpacing: true,
+  printWidth: 80,
+  tabWidth: 2, // 缩进字节数
+  semi: false, // 句尾添加分号
+  useTabs: false, // 缩进是否使用 tab, (false - 空格)
+  singleQuote: true, // 单引号代替双引号
+  bracketSpacing: true, // 在对象之间加空格, true - { foo: bar }
+  trailingComma: 'es5', // 在对象或数组最后一个元素后面是否加逗号
+  arrowParens: 'avoid', // 箭头函数参数只有一个时是否要有小括号, avoid - 省略
+  jsxBracketSameLine: false, // 将多行 JSX 元素的 > 放在最后一行的末尾，而不是单独放在下一行
+  htmlWhitespaceSensitivity: 'ignore', // html 空格敏感度
 }
